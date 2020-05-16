d7449f380d4f99fae78dc810beaf7f4917f6b8f0
nehal076 <nehaljaisalmeria14@gmail.com>
2020-05-16 23:51:38 +0530

    2 files modified, 1 added #17 Local Storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,13 @@ import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.
 import { TodoItemComponent } from './todo-item/todo-item.component';
 import { ListManagerComponent } from './list-manager/list-manager.component';
 import { TodoListService } from './services/todo-list.service';
+import { StorageService } from './services/storage.service';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent, InputButtonUnitComponent, TodoItemComponent, ListManagerComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [TodoListService]
+  providers: [TodoListService, StorageService]
 })
 export class AppModule { }
--- src/app/services/todo-list.service.ts
+++ src/app/services/todo-list.service.ts
@@ -1,10 +1,58 @@
 import { Injectable } from '@angular/core';
 import { TodoItem } from '../interfaces/todo-item';
+import { StorageService } from './storage.service';
 
-@Injectable()
+
+const todoListStorageKey = 'Todo_List';
+
+const defaultTodoList = [
+  {title: 'install NodeJS'},
+  {title: 'install Angular CLI'},
+  {title: 'create new app'},
+  {title: 'serve app'},
+  {title: 'develop app'},
+  {title: 'deploy app'},
+];
+
+@Injectable({
+  providedIn: 'root'
+})
+
 export class TodoListService {
 
-  todoList: TodoItem[] = [
+  todoList: TodoItem[];
+
+  constructor(private storageService: StorageService) {
+    this.todoList = 
+      storageService.getData(todoListStorageKey) || defaultTodoList;
+  }
+
+  addItem(item: TodoItem) {
+    this.todoList.push(item);
+    this.saveList();
+  }
+
+  updateItem(item: TodoItem, changes) {
+    const index = this.todoList.indexOf(item);
+    this.todoList[index] = { ...item, ...changes };
+    this.saveList();
+  }
+
+  saveList() {
+    this.storageService.setData(todoListStorageKey, this.todoList); 
+  }
+
+  deleteItem(item: TodoItem) {
+    const index = this.todoList.indexOf(item);
+    this.todoList.splice(index, 1);
+    this.saveList();
+  }
+
+  getTodoList() {
+    return this.todoList;
+  }
+
+  /*todoList: TodoItem[] = [
     {title: 'install NodeJS'},
     {title: 'install Angular CLI'},
     {title: 'create new app'},
@@ -21,6 +69,6 @@ export class TodoListService {
 
   addItem(item: TodoItem) { 
     this.todoList.push(item);
-  }
+  }*/
 
 }
